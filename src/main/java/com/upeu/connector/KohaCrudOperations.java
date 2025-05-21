1601b64aa4738ee42c45f59615ecdee6b5972817
alberto <atisbo78@gmail.com>
2025-05-21 16:21:19 -0500

    Mejoras

--- src/main/java/com/upeu/connector/KohaCrudOperations.java
+++ src/main/java/com/upeu/connector/KohaCrudOperations.java
@@ -2,11 +2,16 @@ package com.upeu.connector.operations;
 
 import com.upeu.connector.handler.PatronHandler;
 import com.upeu.connector.model.Patron;
+import com.upeu.connector.schema.PatronSchema;
 import com.upeu.connector.util.EndpointRegistry;
+import com.upeu.connector.util.SchemaRegistry;
 import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
+import org.identityconnectors.framework.common.objects.filter.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Set;
 
 public class KohaCrudOperations {
@@ -14,9 +19,11 @@ public class KohaCrudOperations {
     private static final Logger LOG = LoggerFactory.getLogger(KohaCrudOperations.class);
 
     private final EndpointRegistry endpointRegistry;
+    private final SchemaRegistry schemaRegistry;
 
     public KohaCrudOperations(EndpointRegistry endpointRegistry) {
         this.endpointRegistry = endpointRegistry;
+        this.schemaRegistry = new SchemaRegistry(); // Incluye schema __ACCOUNT__
     }
 
     public Uid create(ObjectClass objectClass, Set<Attribute> attributes, OperationOptions options) {
@@ -24,15 +31,11 @@ public class KohaCrudOperations {
             throw new IllegalArgumentException("Soporte solo para tipo __ACCOUNT__");
         }
 
-        PatronHandler handler = new PatronHandler(endpointRegistry) {
-            // Puede extender más adelante si hace falta filtros
-        };
-
+        PatronHandler handler = new PatronHandler(endpointRegistry) {};
         Patron patron = Patron.fromAttributes(attributes);
         Patron created = handler.create(patron);
 
-        // IMPORTANTE: borrowernumber será usado como Uid, cardnumber como Name
-        return new Uid(String.valueOf(created.getBorrowerNumber()));
+        return new Uid(String.valueOf(created.getBorrowernumber()));
     }
 
     public Uid update(ObjectClass objectClass, Uid uid, Set<Attribute> attributes, OperationOptions options) {
@@ -40,19 +43,78 @@ public class KohaCrudOperations {
             throw new IllegalArgumentException("Solo se soporta el tipo __ACCOUNT__");
         }
 
-        PatronHandler handler = new PatronHandler(endpointRegistry) {
-            // Se puede extender si hace falta
-        };
-
+        PatronHandler handler = new PatronHandler(endpointRegistry) {};
         Patron updatedPatron = Patron.fromAttributes(attributes);
-
-        // Se espera que el Uid sea el borrowernumber (ID interno de Koha)
         String borrowerNumber = uid.getUidValue();
 
         Patron result = handler.update(borrowerNumber, updatedPatron);
 
-        // Devuelve el mismo Uid (borrowernumber)
-        return new Uid(String.valueOf(result.getBorrowerNumber()));
+        return new Uid(String.valueOf(result.getBorrowernumber()));
     }
 
+    public void delete(ObjectClass objectClass, Uid uid, OperationOptions options) {
+        if (!objectClass.is(ObjectClass.ACCOUNT_NAME)) {
+            throw new IllegalArgumentException("Solo se soporta el tipo __ACCOUNT__");
+        }
+
+        PatronHandler handler = new PatronHandler(endpointRegistry) {};
+        String borrowerNumber = uid.getUidValue();
+
+        boolean deleted = handler.deleteByBorrowerNumber(borrowerNumber);
+        if (!deleted) {
+            throw new RuntimeException("No se pudo eliminar el usuario con ID " + borrowerNumber);
+        }
+    }
+
+    public void search(ObjectClass objectClass, Filter filter, ResultsHandler handler, OperationOptions options) {
+        if (!objectClass.is(ObjectClass.ACCOUNT_NAME)) {
+            throw new IllegalArgumentException("Soporte solo para tipo __ACCOUNT__");
+        }
+
+        PatronHandler patronHandler = new PatronHandler(endpointRegistry) {
+            @Override
+            public List<Patron> visitEqualsFilter(EqualsFilter filter, Void param) {
+                return super.visitEqualsFilter(filter, param);
+            }
+        };
+
+        List<Patron> results;
+
+        if (filter == null) {
+            results = patronHandler.getAll();
+        } else {
+            results = filter.accept(patronHandler, null);
+        }
+
+        for (Patron patron : results) {
+            ConnectorObject object = buildConnectorObject(patron);
+            handler.handle(object);
+        }
+    }
+
+    public Schema schema() {
+        SchemaBuilder builder = new SchemaBuilder(KohaCrudOperations.class);
+        builder.defineObjectClass(schemaRegistry.getSchema(ObjectClass.ACCOUNT_NAME));
+        return builder.build();
+    }
+
+    private ConnectorObject buildConnectorObject(Patron patron) {
+        ConnectorObjectBuilder builder = new ConnectorObjectBuilder();
+
+        builder.setObjectClass(ObjectClass.ACCOUNT);
+        builder.setUid(patron.getBorrowernumber());
+        builder.setName(patron.getCardnumber());
+
+        // Atributos
+        builder.addAttribute("userid", patron.getUserid());
+        builder.addAttribute("surname", patron.getSurname());
+        builder.addAttribute("firstname", patron.getFirstname());
+        builder.addAttribute("email", patron.getEmail());
+        builder.addAttribute("categorycode", patron.getCategorycode());
+        builder.addAttribute("branchcode", patron.getBranchcode());
+        builder.addAttribute("cardnumber", patron.getCardnumber());
+        builder.addAttribute("borrowernumber", patron.getBorrowernumber());
+
+        return builder.build();
+    }
 }
--- src/main/java/com/upeu/connector/model/Patron.java
+++ src/main/java/com/upeu/connector/model/Patron.java
@@ -1,6 +1,10 @@
 package com.upeu.connector.model;
 
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.Name;
 import org.json.JSONObject;
+
+import java.util.Set;
 
 /**
  * Representa un usuario (patron) de Koha.
@@ -16,8 +20,7 @@ public class Patron {
     private String branchcode;
     private String email;
 
-    public Patron() {
-    }
+    public Patron() {}
 
     public static Patron fromJson(JSONObject json) {
         Patron patron = new Patron();
@@ -29,6 +32,50 @@ public class Patron {
         patron.setCategorycode(json.optString("categorycode", null));
         patron.setBranchcode(json.optString("branchcode", null));
         patron.setEmail(json.optString("email", null));
+        return patron;
+    }
+
+    public static Patron fromAttributes(Set<Attribute> attributes) {
+        Patron patron = new Patron();
+
+        for (Attribute attr : attributes) {
+            String name = attr.getName();
+            Object value = attr.getValue() != null && !attr.getValue().isEmpty() ? attr.getValue().get(0) : null;
+
+            if (value == null) continue;
+
+            switch (name) {
+                case Name.NAME:
+                case "cardnumber":
+                    patron.setCardnumber(value.toString());
+                    break;
+                case "borrowernumber":
+                    patron.setBorrowernumber(value.toString());
+                    break;
+                case "userid":
+                    patron.setUserid(value.toString());
+                    break;
+                case "surname":
+                    patron.setSurname(value.toString());
+                    break;
+                case "firstname":
+                    patron.setFirstname(value.toString());
+                    break;
+                case "categorycode":
+                    patron.setCategorycode(value.toString());
+                    break;
+                case "branchcode":
+                    patron.setBranchcode(value.toString());
+                    break;
+                case "email":
+                    patron.setEmail(value.toString());
+                    break;
+                default:
+                    // Atributo no reconocido, ignorar
+                    break;
+            }
+        }
+
         return patron;
     }
 
